c9eaf40e4cce23060f6f7c239cb99ef64127d4b3
opilane <opilane@HP-CZC2349HT4>
2023-01-18 12:25:56 +0200

    Add project files.

---
+++ Контрольная_работа_по_теме_Циклы.py
@@ -0,0 +1,66 @@
+from math import *
+from random import *
+
+
+print("1Напишите программу, которая по данному числу n от 1 до 9 выводит на экран n елок. Изображение одной елки имеет размер 4×9 символов, между двумя соседними елками также имеется пустой (из пробелов) столбец.")
+print("Разрешается вывести пустой столбец после последней елки. Для упрощения рисования скопируйте елку из примера в среду разработки. Елки должны простоложиться горизонтально.")
+n=randint(1,9)
+for i in range(n):
+    print("   /V\    ")
+    print("  / V \   ")
+    print(" / V V \  ")
+    print("/VV V VV\ ")
+print()
+
+
+
+print("2.Перемножить все не чётные значения в диапазоне от 0 до введенного пользователем числа(R)")
+R=int(input("kirjuta üles täisarv: "))
+p = 1
+for i in range(1, R, 2): p *= i
+print(p)
+print()
+
+
+print("3.Дано N чисел. Найти количество положительных чисел среди них; N рандомное число")
+p=0
+N=randint(1,100)
+for i in range(N):
+    A=randint(-1000,1000)
+    for i in range(1):
+        print(A)
+    if A>0:
+        p+=1
+print("positiivsed numbrid", p)
+print()
+
+
+
+print("4. Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).")
+try:
+    a=int(input("sisestage number: "))
+except:
+    print("Vale andmed")
+b=0
+c=0
+while a>0:
+    if a%2==0:
+        b+=1
+    else:
+        c+=1
+    a=a//10
+print("paarisarvud", b)
+print("paarituid numbreid", c)
+print()
+
+
+
+print("5.Найти сумму ряда чисел от A до B. Полученный результат вывести на экран")
+b=int(input("sisestage teine ​​number: "))
+n=int(input("sisesta viimane number: "))
+i=1
+s=0
+while i<=n:
+    s=i+s
+    i=i+s 
+print("arvude summa alates",i,"kuni",b,"=", (s))
